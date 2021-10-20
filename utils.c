1f69dfc2fceda1627cd6ae50a454254032cbd1f1
Yunjoo Jung <yujung@c7r1s1.42seoul.kr>
2021-10-21 00:38:32 +0900

    re

--- utils.c
+++ utils.c
@@ -1,23 +1,51 @@
 #include "philo.h"
 
+int	check(char ch, unsigned long long ans, int sign)
+{
+	if (ch != '\0' && ('0' > ch || ch > '9'))
+		return (0);
+	if (ans > 9223372036854775807)
+	{
+		if (sign == -1)
+			return (0);
+		else
+			return (-1);
+	}
+	return (ans * sign);
+}
+
+int	ft_atoi(const char *str)
+{
+	unsigned long long	ans;
+	int					sign;
+
+	if (*str == 0)
+		return (0);
+	ans = 0;
+	sign = 1;
+	while (*str == ' ' || *str == '\t' || *str == '\n'
+		|| *str == '\v' || *str == '\f' || *str == '\r')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str && ('0' <= *str && *str <= '9'))
+	{
+		ans = ans * 10 + (*str - '0');
+		str++;
+	}
+	return (check(*str, ans, sign));
+}
+
 int	philo_atoi(char *str)
 {
 	int	result;
 
-	result = 0;
-	while (*str)
-	{
-		if (*str != ' ' || *str != '\t' || *str != '\n'
-			|| *str != '\v' || *str != '\f' || *str != '\r')
-		{
-			if (*str < '0' || *str > '9')
-				return (-1);
-			result *= 10;
-			result += (*str - '0');
-		}
-		str++;
-	}
-	if (result == 0)
+	result = ft_atoi(str);
+	if (result == 0 || result < 0)
 		return (-1);
 	return (result);
 }
